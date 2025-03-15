667fb3fd9d20a2a3ddbeace5102a7bad5ac89434
Nicolas Jonas <nnico+git@pm.me>
2025-03-15 18:20:23 +0100

    fix adminbar greeting

--- php/tweaks/admin-bar-greeting.js
+++ php/tweaks/admin-bar-greeting.js
@@ -2,7 +2,7 @@ const d = document;
 const qs = d.querySelector.bind( d );
 const link = qs( '#wp-admin-bar-my-account > .ab-item' );
 const displayName = qs( '#wp-admin-bar-my-account > .ab-item > .display-name' );
-const greeting = qs( 'script[data-tweakmaster-greeting]' )?.dataset.tweakmasterGreeting;
+const greeting = qs( '[data-tweakmaster-greeting]' )?.dataset.tweakmasterGreeting;
 const avatar = qs( '#wp-admin-bar-my-account > .ab-item > .avatar' );
 
 if ( ! link || ! displayName || ! greeting ) {
