a85b0dd96ac1eedf4d2f28e75b9e55f951684b4e
Nicolas Jonas <nnico+git@pm.me>
2025-10-23 00:14:15 +0200

    fix jq migrate, not sure why

--- php/tweaks-standalone/dequeue-jquery-migrate.php
+++ php/tweaks-standalone/dequeue-jquery-migrate.php
@@ -19,21 +19,19 @@ declare(strict_types = 1);
 
 namespace Nextgenthemes\TweakMaster;
 
-use WP_Scripts;
-
-add_action( 'wp_default_scripts', __NAMESPACE__ . '\dequeue_jquery_migrate', 20 );
+add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\dequeue_jquery_migrate', 15 );
 /**
  * Dequeue jQuery Migrate from the jQuery script dependencies on the frontend.
- *
- * @param WP_Scripts $scripts The WP_Scripts object containing registered scripts.
  *
  * This function checks if the current request is not for the admin area and if
  * the 'jquery' script is registered. If so, it removes 'jquery-migrate' from
  * the dependencies of the 'jquery' script.
  */
-function dequeue_jquery_migrate( WP_Scripts $scripts ): void {
+function dequeue_jquery_migrate(): void {
 
-	if ( ! is_admin() && ! empty( $scripts->registered['jquery'] ) ) {
+	$scripts = wp_scripts();
+
+	if ( ! empty( $scripts->registered['jquery'] ) ) {
 		$jquery_dependencies                 = $scripts->registered['jquery']->deps;
 		$scripts->registered['jquery']->deps = array_diff( $jquery_dependencies, array( 'jquery-migrate' ) );
 	}
